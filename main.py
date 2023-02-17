96edcb57b803937ac2dcb80227055c5a12782fc5
陈俊 <116486681+zcj0125@users.noreply.github.com>
2023-02-17 14:35:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -253,22 +253,24 @@ class RecModel(nn.Module):
         self.dropout2 = nn.Dropout(0.2)
         self.dropout3 = nn.Dropout(0.2)
         self.sigmoid = nn.Sigmoid()
-        self.relu = nn.ReLU()
+        self.relu1 = nn.ReLU()
+        self.relu2 = nn.ReLU()
+        self.relu3 = nn.ReLU()
 
     def forward(self, x):
 
         x = self.fc1(x)
-        x = self.relu(x)
+        x = self.relu1(x)
 
         x = self.dropout1(x)
 
         x = self.fc2(x)
-        x = self.relu(x)
+        x = self.relu2(x)
 
         x = self.dropout2(x)
 
         x = self.fc3(x)
-        x = self.relu(x)
+        x = self.relu3(x)
 
         x = self.dropout3(x)
 
